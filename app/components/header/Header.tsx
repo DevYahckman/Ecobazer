b9c1d673f933667b98206d4df83696740c30f544
yahckman <adeniranajibade2014@gmail.com>
2024-10-01 13:20:41 -0700

    appBar on mibile

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,56 +1,115 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import useCheckIsMobile from "@/app/hooks/useCheckIsMobile";
 import { SlLocationPin } from "react-icons/sl";
 import { FiHeart } from "react-icons/fi";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { FiPhoneCall } from "react-icons/fi";
 // import { GrSearch } from "react-icons/gr";
+import { FaHome,FaBloggerB, FaInfoCircle   } from "react-icons/fa";
+import { FaShop, FaMapLocationDot  } from "react-icons/fa6";
+import { HiOutlineMenuAlt3 } from "react-icons/hi"
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/app/assets/svg/logo.svg";
+import userImage from "@/app/assets/images/customer-2.png";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 
 interface navProps {
   name: string;
   path: string;
+  icon: any
+  // icon: JSX.Element
 }
 
 const AppHeader = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const isMobile = useCheckIsMobile();
+
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   const navItem: navProps[] = [
     {
       name: "Home",
       path: "#",
+      icon:<FaHome  size={19}/>
+      
     },
     {
       name: "Shop",
-      path: "#",
+      path: "/shop",
+      icon:<FaShop size={19}/>
     },
-    {
-      name: "Pages",
-      path: "#",
-    },
+
     {
       name: "Blog",
       path: "#",
+      icon:<FaBloggerB size={19}/>
     },
     {
       name: "About Us",
       path: "#",
+      icon:<FaInfoCircle size={19}/>
     },
     {
       name: "Contact Us",
       path: "#",
+      icon:<FaMapLocationDot size={19}/>
     },
   ];
   return (
     <div className="">
       {isMobile ? (
-        <div>mobile</div>
+        <div>
+          <div className="flex justify-between items-center px-2 py-4 bg-deepGray">
+            <div className="flex space-x-3 items-center">
+              <Image src={logo} alt="logo" />
+              <p className="font-bold text-3xl text-white">Ecobazar</p>
+            </div>
+
+            <div className="flex text-deepGray items-center space-x-6">
+              <button onClick={toggleDrawer}>
+              <HiOutlineMenuAlt3 size={30} className=" text-white"/>
+              </button>
+
+              <Drawer
+                open={isOpen}
+                onClose={toggleDrawer}
+                direction="left"
+                className="p-5"
+                size={300}
+              >
+                <div className="flex items-start space-x-2" >
+                  <Image src={userImage} alt="" className="rounded-full w-12" />
+                  <div>
+                    <p className="font-bold text-deepGray ">Cornor McGregor</p>
+                    <p className="text-lightGray font-medium">
+                      mcgregor@gmail.com
+                    </p>
+                  </div>
+                </div>
+                <hr className="my-4" />
+
+                {navItem.map((item, i: number) => (
+                  <div className="flex items-center space-x-5 my-5" onClick={()=>{setIsOpen(false)}}>
+                    <span className="">
+
+                    {item.icon}
+                    </span>
+                    <p className="font-medium text-lg text-lightGray">{item.name}</p>
+                  </div>
+                ))}
+              </Drawer>
+            </div>
+          </div>
+        </div>
       ) : (
         <div>
           {/* Top */}
-          <div className="flex justify-between  items-center  bg-deepGray px-7 py-2 text-gray-300 text-xs">
+          <div className=" hidden lg:flex justify-between  items-center  bg-deepGray px-7 py-2 text-gray-300 text-xs">
             <div className="flex items-center space-x-2 ">
               <SlLocationPin size={18} />
               <p>Store Location: Lincoln- 344, Illinois, Chicago, USA</p>
@@ -66,7 +125,7 @@ const AppHeader = () => {
             </div>
           </div>
           {/* Mid */}
-          <div className="flex justify-between items-center px-7 py-6">
+          <div className="flex justify-between items-center px-7 py-4">
             <div className="flex space-x-3 items-center">
               <Image src={logo} alt="logo" />
               <p className="font-bold text-3xl">Ecobazar</p>
@@ -81,7 +140,6 @@ const AppHeader = () => {
                  Search
                  <button className="bg-success p-4">Search</button>
                  </div> */}
-
             </div>
 
             <div className="flex text-deepGray items-center space-x-6">
@@ -110,7 +168,7 @@ const AppHeader = () => {
             <div className="flex space-x-14 ">
               {navItem.map((item, i: number) => (
                 <Link
-                  href={"#"}
+                  href={item.path}
                   className="text-gray-500 text-sm font-bold"
                   key={i}
                 >
@@ -119,12 +177,14 @@ const AppHeader = () => {
               ))}
             </div>
             <div className="flex items-center space-x-2 text-deepGray font-bold">
-             
               <FiPhoneCall size={20} />
               <div>+34 9474 3434</div>
             </div>
           </div>
         </div>
+
+
+
       )}
     </div>
   );
